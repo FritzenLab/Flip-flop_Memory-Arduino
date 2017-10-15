f54129c41c7e6378099b9f8207e3ffca600d81ab
clovisf <clovisf@gmail.com>
2017-10-15 21:53:32 -0200

    flip flop RS

--- Flip-flop_Memory-Arduino/flip-flops-memory-arduino/flip-flops-memory-arduino.ino
+++ Flip-flop_Memory-Arduino/flip-flops-memory-arduino/flip-flops-memory-arduino.ino
@@ -1,9 +1,63 @@
+#define Q1output 2
+#define Q1invertedOutput 3
+
+#define InputSet 4
+#define InputReset 5
+
+boolean SetInputState;
+boolean ResetInputState;
+boolean Q1state = true;
+
+ 
 void setup() {
-  // put your setup code here, to run once:
+  // initialize the LED pin as an output:
+  pinMode(Q1output, OUTPUT);
+  pinMode(Q1invertedOutput, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(InputSet, INPUT);
+  pinMode(InputReset, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // NAND R-S (Reset-Set) Flip flop
+  
+  SetInputState = digitalRead(InputSet);
+  ResetInputState = digitalRead(InputReset);
+  delay(50);
+
+  if (Q1state == true){
+    if ((SetInputState == false) && (ResetInputState == false)){
+        digitalWrite(Q1output,true);
+        digitalWrite(Q1invertedOutput, false);
+    } else if ((SetInputState == false) && (ResetInputState == true)){
+        digitalWrite(Q1output,false);
+        digitalWrite(Q1invertedOutput, true);
+        Q1state = true;    
+    } else if ((SetInputState == true) && (ResetInputState == false)){
+        digitalWrite(Q1output, true);
+        digitalWrite(Q1invertedOutput, false);
+        Q1state = false;    
+    } else if ((SetInputState == true) && (ResetInputState == true)){
+         
+    }
+  } else{
+    if ((SetInputState == false) && (ResetInputState == false)){
+        digitalWrite(Q1output, false);
+        digitalWrite(Q1invertedOutput, true);
+    } else if ((SetInputState == false) && (ResetInputState == true)){
+        digitalWrite(Q1output, false);
+        digitalWrite(Q1invertedOutput, true);
+        Q1state = true;    
+    } else if ((SetInputState == true) && (ResetInputState == false)){
+        digitalWrite(Q1output, true);
+        digitalWrite(Q1invertedOutput, false);
+        Q1state = false;    
+    } else if ((SetInputState == true) && (ResetInputState == true)){
+        
+    }
+  }
+  
+  
 }
